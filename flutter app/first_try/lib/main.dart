f2df6a278d1c84870f76a59e453edd781a3795c8
Mohak5142 <mohakkhemu@gmail.com>
2026-04-12 16:39:02 +0530

    uploading my flutter project to github

---
+++ flutter app/first_try/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+    runApp(
+         MaterialApp(
+            home: Scaffold(
+                body: Container(
+                    decoration: BoxDecoration( ),
+                    child: const Center(
+                    child: Text('Hello World!'),
+                  ),
+                ),
+            ),
+        ),
+    );
+}
